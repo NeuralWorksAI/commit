3b75f82133e8f9e0722d40a8430bcffeca3ef88e
Piyush Goel <piyushgoel2008@gmail.com>
2021-05-01 02:20:23 +0530

    AppBar and NavBar added

--- commit/lib/main.dart
+++ commit/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'my_flutter_app_icons.dart';
+
 void main() {
   runApp(MyApp());
 }
@@ -50,19 +52,6 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     // This method is rerun every time setState is called, for instance as done
@@ -73,45 +62,43 @@ class _MyHomePageState extends State<MyHomePage> {
     // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
+        title: Row(
           mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+          children: [
+            Image.asset(
+              'assets/pd.png',
+              fit: BoxFit.contain,
+              height: 80,
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
+            Container(
+                padding: const EdgeInsets.all(8.0),
+                child: Text('Payment Information'))
           ],
         ),
+        toolbarHeight: 120.0,
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      bottomNavigationBar: BottomNavigationBar(
+          items: const <BottomNavigationBarItem>[
+            BottomNavigationBarItem(
+                icon: Icon(MyFlutterApp.friends),
+                label: 'Friends',
+                backgroundColor: Colors.white),
+            BottomNavigationBarItem(
+                icon: Icon(MyFlutterApp.home),
+                label: ' ',
+                backgroundColor: Colors.white),
+            BottomNavigationBarItem(
+              icon: Icon(MyFlutterApp.activity),
+              label: 'Activity',
+              backgroundColor: Colors.white,
+            ),
+          ],
+          type: BottomNavigationBarType.fixed,
+          // currentIndex: _selectedIndex,
+          //selectedItemColor: Colors.black,
+          iconSize: 40,
+          //onTap: _onItemTapped,
+          elevation: 5),
     );
   }
 }
