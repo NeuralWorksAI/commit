0703a7328d2b46aca9c3d4b537e86b2a0beea0e3
00MB <s2061204@ed.ac.uk>
2021-05-01 00:18:19 +0100

    initial setup

---
+++ lib/screens/wrapper.dart
@@ -0,0 +1,10 @@
+import 'package:commit/screens/home/home.dart';
+import 'package:flutter/material.dart';
+
+class Wrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    //return either home or authenticate
+    return Home();
+  }
+}
